344ebc85671d23b47d1531a27ddb26a0bc02703e
DELMUS1M <ogoradelmus1@gmail.com>
2022-09-08 09:24:21 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -20,10 +20,8 @@ int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
 
 }
-
-
