5248dda326b6a8b0795030dd36a7e9e6231ab822
DELMUS1M <ogoradelmus1@gmail.com>
2022-09-09 13:34:34 -0700

    Task 8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,16 @@
  */
 int main(void)
 {
-	char a;
+	char sm_letter = 'a';
+	int num = 48;
 
-	for (a = '0'; a >= 'F' ; a--)
-		putchar(a);
-	putchar('\n');
+	for (; sm_letter <= 'f'; sm_letter++)
+	{
+		for (; num < 58; num++)
+			putchar(num);
+		putchar(sm_letter);
+		if (sm_letter == 'f')
+			putchar('\n');
+	}
 	return (0);
 }
