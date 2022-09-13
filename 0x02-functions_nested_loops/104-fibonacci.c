f7d18d5e4c9d014c7f0d183f8d229ff7f0de7865
DELMUS1M <ogoradelmus1@gmail.com>
2022-09-13 09:48:48 -0700

    Task 16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Return: nothing
+ * main - prints out first 98
+ * fibonacci suit numbers
+ * Return: return 0
  */
 int main(void)
 {
-	int counter = 2;
+	int inc;
+	unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+	unsigned long h1, h2;
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
-
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
+	for (inc = 0; inc < 92; inc++)
 	{
-		counter++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (counter < 98)
+		n3 = n1 + n2;
+		printf("%lu, ", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+	n1_h1 = n1 / 10000000000;
+	n2_h1 = n2 / 10000000000;
+	n1_h2 = n1 % 10000000000;
+	n2_h2 = n2 % 10000000000;
+	for (inc = 93; inc < 99; inc++)
+	{
+		h1 = n1_h1 + n2_h1;
+		h2 = n1_h2 + n2_h2;
+		if ((n1_h2 + n2_h2) > 9999999999)
 		{
-			printf(", ");
+			h1 += 1;
+			h2 %= 10000000000;
 		}
+		printf("%lu%lu", h1, h2);
+		if (inc != 98)
+			printf(", ");
+
+		n1_h1 = n2_h1;
+		n1_h2 = n2_h2;
+		n2_h1 = h1;
+		n2_h2 = h2;
 	}
 	printf("\n");
 	return (0);
 }
+
