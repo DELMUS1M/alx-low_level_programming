c9fc1979ea13505f73fe68bf90d9bb19ae3857c8
DELMUS1M <ogoradelmus1@gmail.com>
2022-09-19 09:29:52 -0700

    Task 0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ * Project : 0. 98 Battery st.
+ * Programmer: Delmus Ogora
+ * For  ALX school 2022 August cohort
+ */
+
+#include "main.h"
+
+/*
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
