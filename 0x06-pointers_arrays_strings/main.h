a0718f79eb94b031b5c10ae88d91e674ea8b8ef0
DELMUS1M <ogoradelmus1@gmail.com>
2022-09-21 09:44:10 -0700

    Task 6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,17 +2,16 @@
 #define _MAIN_H_INCLUDED
 
 int _putchar(char);
-char *_strcat(char *, char *);
-char *_strncat(char *,char *,int);
-char *_strncpy(char *, char *, int);
-int _strcmp(char *, char *);
-void reverse_array(int *,int);
-char *string_toupper(char *);
-char *cap_string(int *str);
+char *_strcat(char *dest, char *src);
+char *_strcat(char*, char*);
+char *_strncat(char*, char*, int);
+char *_strncpy(char*, char*, int);
 char *leet(char *);
-char *rot13(char *);
-void print_number(int);
-char *infinite_add(char *, char *,char *, int);
-void print_buffer(char *, int);
+int _strcmp(char*, char*);
+void reverse_array(int*, int);
+char *string_toupper(char*);
+char *cap_string(char*);
+char *rot13(char*);
+void print_number(int n);
 
 #endif
