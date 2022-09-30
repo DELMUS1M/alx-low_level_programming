7ed4c45c3b94a129cf802b45083f2557d21d4501
DELMUS1M <ogoradelmus1@gmail.com>
2022-09-30 07:23:55 -0700

    Task 5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,50 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
 /**
- * main - prints the minimum number of coins
- * to make change for an amount of money
- * @argc: n args
- * @argv: arr args
- * Return: 0
+ * main - Main Entry
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	int value, c;
+	int cents, coins = 0;
 
-	c = 0;
-	if (argc != 2)
+	if (argc == 2)
 	{
-		printf("Error\n");
-		return (0);
+		cents = atoi(*(argv + 1));
+		while (cents > 0)
 	}
-	value = atoi(argv[1]);
-	if (value < 0)
+	if (cents % 25 < cents)
 	{
-		printf("%d\n", 0);
-		return (0);
+		cents -= 25;
+		coins++;
 	}
-	if (value % 25 >= 0)
+	else if (cents % 10 < cents)
 	{
-		c += value / 25;
-		value = value % 25;
+		cents -= 10;
+		coins++;
 	}
-	if (value % 10 >= 0)
+	else if (cents % 5 < cents)
 	{
-		c += value / 5;
-		value = value % 5;
+		cents -= 5;
+		coins++;
 	}
-	if (value % 2 >= 0)
+	else if (cents % 2 < cents)
 	{
-		c += value / 2;
-		value = value % 2;
+		cents -= 2;
+		coins++;
 	}
-	if (value % 1 >= 0)
+	else if (cents % 1 < cents)
 	{
-		c += value / 1;
+		cents -= 1;
+		coins++;
 	}
-	printf("%d\n", c);
-	return (0);
+}
+	else
+{
+	printf("Error\n");
+	return (1);
+}
+printf("%d\n", coins);
+return (0);
 }
