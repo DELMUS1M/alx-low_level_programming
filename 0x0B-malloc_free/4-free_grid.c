0997abb95db1192abb22bae293379af45e693831
DELMUS1M <ogoradelmus1@gmail.com>
2022-10-03 08:08:48 -0700

    Task 4. It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
  * free_grid - frees a 2 dimensional grid previously
  * created by the alloc_grid function
  * @grid: double pointer to multi-dimen array
- * height: height of grid
+ * @height: height of grid
  */
 void free_grid(int **grid, int height)
 {
