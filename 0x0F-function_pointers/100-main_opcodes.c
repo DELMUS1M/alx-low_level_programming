7159dab247f7a4aea840024c7772ff2c13a664cd
DELMUS1M <ogoradelmus1@gmail.com>
2022-10-13 07:56:34 -0700

    Task 4. Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,45 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "function_pointers.h"
 /**
- * main -  program that prints the opcodes of its own main function.
- * @argc: number of arguments passed to the program
- * @argv: array of arguments
- *
- * Return: on success, 1 or 2 in case of failure
+ * main - define function
+ * @void - describe argument
+ * Return: what does it return?
  */
-
-int main(int argc, char *argv[])
+int main(void)
 {
-
-	int index, bytes;
-	int (*address)(int, char **) = main;
-	unsigned char opcode;
-
-	if (argc != 2)
-}
-printf("Error\n");
-exit(1);
-}
-
-bytes = atoi(argv[1]);
-
-if (bytes < 0)
-{
-	printf("Error\n");
-	exit(2);
-}
-
-for (index = 0; index <  bytes; index++)
-{
-	opcode = *(unsigned char *)address;
-	printf("%.2x", opcode);
-
-	if (index == bytes - 1)
-		continue;
-	printf(" ");
-	address++;
-}
-printf("\n");
-return (0);
+	return (0);
 }
