6ac56a428da74b381656db1f524400bcc1937ee2
DELMUS1M <ogoradelmus1@gmail.com>
2022-10-14 11:19:42 -0700

    Task 3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -36,9 +36,6 @@ void print_all(const char * const format, ...)
 			case 'f':
 				printf("%f", va_arg(valist, double)), c = 1;
 				break;
-			case 'f':
-				printf("%f", va_arg(valist, double)), c = 1;
-				break;
 			case 's':
 				str = va_arg(valist, char *), c = 1;
 				if (!str)
